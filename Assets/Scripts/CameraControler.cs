2e55e5bb513bb32f13a112c10399619e8a4c1073
RutekGutek <RutekGutek@users.noreply.github.com>
2023-09-18 19:56:09 +0200

    18.09.2023

--- Assets/Scripts/CameraControler.cs
+++ Assets/Scripts/CameraControler.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class CameraControler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private float mouseSensitivity = 500f;
+
+    private Transform playerBody;
+    private float xRotation = 0f;
+
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        playerBody = transform.parent;
     }
 
-    // Update is called once per frame
+    private void CameraRotation() {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+
+
     void Update()
     {
-        
+        CameraRotation();
     }
 }
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    [SerializeField] private float speed = 12f;
+    private Vector3 velocity;
+
+    private CharacterController characterController;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void PlayerMove() {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        characterController.Move(move * speed * Time.deltaTime);
+    }
+
+
+
+    void Update()
+    {
+        PlayerMove();
+    }
+}
